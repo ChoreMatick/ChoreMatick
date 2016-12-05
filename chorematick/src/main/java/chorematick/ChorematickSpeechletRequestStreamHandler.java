c614e7312d4ee922d77143b76c1f26ac518a7a6f
Chris Lawrence <chris.lawrence@gmx.com>
2016-12-05 18:38:34 +0000

    compile with App ID entered in '..StreamHandler'

--- chorematick/src/main/java/chorematick/ChorematickSpeechletRequestStreamHandler.java
+++ chorematick/src/main/java/chorematick/ChorematickSpeechletRequestStreamHandler.java
@@ -8,7 +8,7 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 public final class ChorematickSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
     private static final Set<String> supportedApplicationIds = new HashSet<String>();
     static {
-        supportedApplicationIds.add("****AlexaSkillID****");
+        supportedApplicationIds.add("amzn1.ask.skill.6ff2efd2-c737-49e4-9dc2-6c33dd33eb9d");
     }
 
     public ChorematickSpeechletRequestStreamHandler() {
