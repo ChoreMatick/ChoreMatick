b8b619ffba0b853373751fa03dd62dff1cc4fef2
Dagmara <daga.sz@hotmail.com>
2016-12-08 16:52:03 +0000

    Wrote tests, started on task class

---
+++ chorematick/src/test/java/chorematick/TaskListTest.java
@@ -0,0 +1,20 @@
+package chorematick;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.mockito.Mock;
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+import static org.hamcrest.CoreMatchers.*;
+
+public class TaskListTest extends BaseTestCase {
+  @Test
+  public void getTasksTest() {
+    
+  }
+  @Test
+  public void getDaysTaskTest() {
+
+  }
+}
